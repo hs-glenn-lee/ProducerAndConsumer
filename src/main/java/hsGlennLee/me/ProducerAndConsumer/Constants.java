5955021a4830483487fbb2a039ac8e15ad9e2bf4
hs-glenn-lee <git_test@mail.com>
2018-09-29 20:05:57 +0900

    fix Constants

--- src/main/java/hsGlennLee/me/ProducerAndConsumer/Constants.java
+++ src/main/java/hsGlennLee/me/ProducerAndConsumer/Constants.java
@@ -1,7 +1,6 @@
 package hsGlennLee.me.ProducerAndConsumer;
 
 public class Constants {
-	public static final String targetDirectoryPathString = "/1";
-	//public static final String targetDirectoryPathString = "/pac-generated-data";
+	public static final String targetDirectoryPathString = "/pac-generated-data";
 	public static final String sorceWordsPathString = "./SourceWords";
 }
